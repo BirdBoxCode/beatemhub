f9268df22e492004c0d43eb11c4c4ff593fdbbd1
Chris McCarthy <chmc87@gmail.com>
2022-10-31 20:17:26 +0100

    fine tunes

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// window.scroll({
+//   top: 2500,
+//   left: 0,
+//   behavior: 'smooth'
+// });
+
+// Scroll certain amounts from current position
+// window.scrollBy({
+//   top: 100, // could be negative value
+// //   left: 0,
+// //   behavior: 'smooth'
+// // });
+
+// document.querySelector('.bgimg-1').scrollIntoView({
+//   behavior: 'smooth'
+// });
