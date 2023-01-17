8dbb3c48310319583cd2e362dde1fe6fd6e7f144
Christopher McCarthy <life@ChristophersMBP.fritz.box>
2023-01-17 10:47:29 +0100

    scroll on click

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//burger bar
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
@@ -8,43 +11,42 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-// function onClickHandler(e){
-//   var content = document.getElementById("body");
 
-//   if(hamburger-icon.checked){
-//     content.setAttribute( 'body', 'push-down' );
+// scroll on click ABOUT
+function slowScrollToElement(elementId) {
+  if (elementId === 'aboutarea') {
+    var element = document.getElementById('aboutarea');
+    var elementPosition = element.getBoundingClientRect().top;
+  } else if (elementId === 'servicesarea') {
+    var element = document.getElementById('servicesarea');
+    var elementPosition = element.getBoundingClientRect().top;
+  } else if (elementId === 'events') {
+    var element = document.getElementById('events');
+    var elementPosition = element.getBoundingClientRect().top;
+  } else if (elementId === 'clientarea') {
+    var element = document.getElementById('clientarea');
+    var elementPosition = element.getBoundingClientRect().top;
+  } else if (elementId === 'contactarea') {
+    var element = document.getElementById('contactarea');
+    var elementPosition = element.getBoundingClientRect().top;
+  }
+    var startPosition = window.pageYOffset;
+    var distance = elementPosition - startPosition;
+    var duration = 1200;
+    var start = null;
 
-//   }else{
-//     content.setAttribute( 'body', 'push-down' );
-//   }
-// }
+  window.requestAnimationFrame(step);
 
-// Title scroll into view
-// const element = document.getElementById("caption");
-
-// element.scrollIntoView();
-// element.scrollIntoView(false);
-// element.scrollIntoView({block: "end"});
-// element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-
-// window.scroll({
-//   top: 2500,
-//   left: 0,
-//   behavior: 'smooth'
-// });
-
-// Smooth scroll to element on link click
-
-// var element = document.getElementById("aboutarea");
-// element.scrollIntoView({behavior: "smooth"});
-
-// $(document).ready(function() {
-//   $("a.aboutarea").click(function(event) {
-//       event.preventDefault();
-//       $("html, body").animate({
-//           scrollTop: $($(this).attr("#aboutarea")).offset().top
-//         }, 500);
-//     });
-// });
-
-// Smooth scroll
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+function easeInOutCubic(t, b, c, d) {
+  t /= d/2;
+  if (t < 1) return c/2*t*t*t + b;
+  t -= 2;
+  return c/2*(t*t*t + 2) + b;
+}
