a839aed7ba7962f3f611fb2223a974b7277d5bd9
Christopher McCarthy <life@MacBookPro.fritz.box>
2026-01-22 12:40:45 +0100

    layout fixes

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,7 +5,7 @@ import { SectionWrapper } from '@/components/SectionWrapper';
 
 export function Introduction() {
   return (
-    <SectionWrapper id="intro" lineColor="pink">
+    <SectionWrapper id="intro" lineColor="pink" className="-mt-[100px] relative z-10">
       <div className="flex flex-col gap-10">
         
         {/* Main Heading */}
@@ -20,9 +20,9 @@ export function Introduction() {
         <div className="flex flex-col gap-6">
             <p className="p3 max-w-2xl">Create your ultimate gaming space.</p>
             
-            <Link href="#contact" className="inline-flex items-center gap-4 bg-violet rounded-[10px] px-4 py-3 w-fit hover:opacity-90 transition-opacity">
-                <Image src="/images/black-button-line.png" alt="" width={24} height={24} />
-                <span className="text-black font-bold font-roboto text-lg tracking-wider">Get in touch with us!</span>
+            <Link href="#contact" className="inline-flex items-center gap-2 bg-violet rounded-[6px] px-4 py-2 w-fit hover:opacity-90 transition-opacity">
+                <Image src="/images/black-button-line.png" alt="" width={4} height={4} />
+                <span className="text-black font-bold font-roboto text-[18px] tracking-wider">Get in touch with us!</span>
             </Link>
         </div>
 
